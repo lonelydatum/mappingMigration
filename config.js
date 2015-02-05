00c961fec3d48d8aa0d0558c643f502c8b738020
Gar Liu <web@garliu.com>
2015-02-05 17:14:42 -0500

    first

---
+++ config.js
@@ -0,0 +1,64 @@
+System.config({
+  "paths": {
+    "*": "*.js",
+    "app/*": "lib/*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "meta": {
+    "jspm_packages/github/mrdoob/three.js@master/build/three": {
+      "format": "global"
+    },
+    "jspm_packages/github/greensock/GreenSock-JS@1.15.1/src/minified/TweenLite.min": {
+      "format": "global"
+    }
+  }
+});
+
+System.config({
+  "map": {
+    "TweenLite": "jspm_packages/github/greensock/GreenSock-JS@1.15.1/src/minified/TweenLite.min",
+    "signals": "github:millermedeiros/js-signals@1.0.0",
+    "jsonp": "npm:jsonp@0.1.0",
+    "stats.js": "github:mrdoob/stats.js@master",
+    "three": "jspm_packages/github/mrdoob/three.js@master/build/three",
+    "github:jspm/nodelibs-fs@0.1.0": {
+      "assert": "npm:assert@1.3.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.0"
+    },
+    "github:jspm/nodelibs-process@0.1.0": {
+      "process": "npm:process@0.10.0"
+    },
+    "github:jspm/nodelibs-tty@0.1.0": {
+      "tty-browserify": "npm:tty-browserify@0.0.0"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:debug@2.1.1": {
+      "fs": "github:jspm/nodelibs-fs@0.1.0",
+      "ms": "npm:ms@0.6.2",
+      "net": "github:jspm/nodelibs-net@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.0",
+      "tty": "github:jspm/nodelibs-tty@0.1.0",
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:jsonp@0.1.0": {
+      "debug": "npm:debug@2.1.1"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.0"
+    }
+  }
+});
+
