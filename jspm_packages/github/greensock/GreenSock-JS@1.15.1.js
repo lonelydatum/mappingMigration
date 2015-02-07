43bbe190eb187ccd1768ce0352bb1242c17bef4e
gar <web@garliu.com>
2015-02-06 21:12:24 -0500

    library

---
+++ jspm_packages/github/greensock/GreenSock-JS@1.15.1.js
@@ -0,0 +1,1 @@
+module.exports = require("github:greensock/GreenSock-JS@1.15.1/src/uncompressed/TweenMax");
---
+++ jspm_packages/github/millermedeiros/js-signals@1.0.0.js
@@ -0,0 +1,3 @@
+define(["github:millermedeiros/js-signals@1.0.0/dist/signals"], function(main) {
+  return main;
+});
